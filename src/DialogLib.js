7a50847e3255a43514be5cdd0a9ab79c05a12369
John Murphy <John@Astronomy.connect>
2020-01-22 17:08:03 +0000

    Created DialogLib.js library so that common dialog code could be factored out of the javascript programs

---
+++ src/DialogLib.js
@@ -0,0 +1,106 @@
+// Version 1.0 (c) John Murphy 20th-Oct-2019
+//
+// ======== #license ===============================================================
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, version 3 of the License.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <http://www.gnu.org/licenses/>.
+// =================================================================================
+//"use strict";
+#include <pjsr/FrameStyle.jsh>
+#include <pjsr/NumericControl.jsh>
+#include <pjsr/Sizer.jsh>
+#include <pjsr/TextAlign.jsh>
+#include <pjsr/StdButton.jsh>
+#include <pjsr/StdIcon.jsh>
+#include <pjsr/StdCursor.jsh>
+
+/**
+ * @param {String} text
+ * @returns {Label} label in FrameStyle_Box
+ */
+function createTitleLabel(text){
+    let titleLabel = new Label();
+    titleLabel.frameStyle = FrameStyle_Box;
+    titleLabel.margin = 4;
+    titleLabel.wordWrapping = true;
+    titleLabel.useRichText = true;
+    titleLabel.text = text;
+    return titleLabel;
+}
+
+/**
+ * Create HorizontalSizer that contains newInstance, documentation, Cancel & OK buttons
+ * @param {type} dialog
+ * @param {type} data
+ * @param {type} newInstanceIcon
+ * @param {type} helpMsg
+ * @returns {HorizontalSizer}
+ */
+function createWindowControlButtons(dialog, data, newInstanceIcon, helpMsgTitle, helpMsg){
+    let ok_Button = new PushButton();
+    ok_Button.text = "OK";
+    ok_Button.cursor = new Cursor(StdCursor_Checkmark);
+    ok_Button.onClick = function () {
+        dialog.ok();
+    };
+
+    let cancel_Button = new PushButton();
+    cancel_Button.text = "Cancel";
+    cancel_Button.cursor = new Cursor(StdCursor_Crossmark);
+    cancel_Button.onClick = function () {
+        dialog.cancel();
+    };
+
+    let buttons_Sizer = new HorizontalSizer;
+    buttons_Sizer.spacing = 6;
+
+    // New Instance button
+    let newInstance_Button = new ToolButton();
+    newInstance_Button.icon = newInstanceIcon;
+    newInstance_Button.setScaledFixedSize(24, 24);
+    newInstance_Button.toolTip = "Save as Process Icon";
+    newInstance_Button.onMousePress = function () {
+        this.hasFocus = true;
+        this.pushed = false;
+        data.saveParameters();
+        dialog.newInstance();
+    };
+
+    let browseDocumentationButton = new ToolButton();
+    browseDocumentationButton.icon = ":/process-interface/browse-documentation.png";
+    browseDocumentationButton.toolTip =
+            "<p>Opens a browser to view the script's documentation.</p>";
+    browseDocumentationButton.onClick = function () {
+        (new MessageBox(
+                helpMsg,
+                helpMsgTitle,
+                StdIcon_Information,
+                StdButton_Ok
+                )).execute();
+    };
+
+    buttons_Sizer.add(newInstance_Button);
+    buttons_Sizer.add(browseDocumentationButton);
+
+    let resetButton = new ToolButton();
+
+    resetButton.icon = ":/images/icons/reset.png";
+    resetButton.toolTip = "<p>Resets the dialog's parameters.";
+    resetButton.onClick = function () {
+        data.resetParameters(dialog);
+    };
+
+    buttons_Sizer.add(resetButton);
+    buttons_Sizer.addStretch();
+    buttons_Sizer.add(ok_Button);
+    buttons_Sizer.add(cancel_Button);
+    return buttons_Sizer;
+}
